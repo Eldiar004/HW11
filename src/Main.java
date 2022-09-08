40ef349a6d6cfca2bb3a1a4aa1ccf6fd59ed906a
Eldiar004 <eldiarro2004@gmail.com>
2022-09-08 21:54:03 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.getArea(5,5,5);
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle {
+    int a,b,c;
+    public void getArea(int a,int b,int c){
+        System.out.println(a+b+c);
+    }
+}
